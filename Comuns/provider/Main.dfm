4e76ce9e38f0f8be6c2f369d734538be2c091b10
Jose Thiago Da Costa Marques <134023216+ciafelina@users.noreply.github.com>
2024-09-25 17:01:39 -0300

    2509-1701

---
+++ Comuns/provider/Main.dfm
@@ -0,0 +1,7 @@
+inherited FormMain: TFormMain
+  BorderIcons = [biSystemMenu, biMinimize]
+  Caption = 'FormMain'
+  WindowState = wsMaximized
+  PixelsPerInch = 96
+  TextHeight = 15
+end
---
+++ Comuns/provider/Main.pas
@@ -0,0 +1,24 @@
+unit Main;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, ViewBase, Vcl.ExtCtrls, Vcl.StdCtrls;
+
+type
+  TFormMain = class(TfrmViewBase)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FormMain: TFormMain;
+
+implementation
+
+{$R *.dfm}
+
+end.
